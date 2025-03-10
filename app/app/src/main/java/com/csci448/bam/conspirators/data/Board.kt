2f79a8b2ccc0038474ffec7006b63ce55eaa4c82
Mallorie Mackay <malloriem123@gmail.com>
2025-03-10 17:46:31 -0500

    some extra framework

---
+++ app/app/src/main/java/com/csci448/bam/conspirators/data/Board.kt
@@ -0,0 +1,7 @@
+package com.csci448.bam.conspirators.data
+
+import java.util.UUID
+
+data class Board(var thumbnailImgId: Int, var nameId: Int, val userUUID: UUID) {
+
+}
---
+++ app/app/src/main/java/com/csci448/bam/conspirators/data/User.kt
@@ -0,0 +1,7 @@
+package com.csci448.bam.conspirators.data
+
+import java.util.UUID
+
+data class User(var userNameId: Int, val userId: UUID) {
+    
+}
