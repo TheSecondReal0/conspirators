c0077b75d50b055eb272abf116eaefd9a2b1333e
Mallorie Mackay <malloriem123@gmail.com>
2025-03-10 18:18:54 -0500

    skrings

--- app/app/src/main/java/com/csci448/bam/conspirators/data/Board.kt
+++ app/app/src/main/java/com/csci448/bam/conspirators/data/Board.kt
@@ -2,6 +2,6 @@ package com.csci448.bam.conspirators.data
 
 import java.util.UUID
 
-data class Board(var thumbnailImgId: Int, var nameId: Int, val userUUID: UUID) {
+data class Board(var thumbnailImgId: Int, var name: String, val userUUID: UUID) {
 
 }
