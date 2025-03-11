26b2786cb5646cee94d5196098f548c179431998
Mallorie Mackay <malloriem123@gmail.com>
2025-03-10 19:20:22 -0500

    oops

--- app/app/src/main/java/com/csci448/bam/conspirators/data/User.kt
+++ app/app/src/main/java/com/csci448/bam/conspirators/data/User.kt
@@ -2,6 +2,6 @@ package com.csci448.bam.conspirators.data
 
 import java.util.UUID
 
-data class User(var userName: String, val userId: UUID) {
+data class User(var userName: String, val userId: UUID, var profilePicId: Int) {
 
 }
